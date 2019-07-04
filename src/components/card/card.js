d410f6b089cac4aa772ad70a9995e51bb9cf84f2
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2019-07-04 19:42:58 -0400

    Some cards

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,28 +7,6 @@ import stone_2 from './../../img/stone_2.png';
 import stone_or_stick from './../../img/stone_or_stick.png';
 import mud from './../../img/mud.png';
 import water from './../../img/water.png'
-import wolf from './../../img/wolf.png';
+import wolf_brick from './../../img/wolf_brick.png'; // needs all 3
 import glass from './../../img/glass.png';
 
-phase1Cards = 20;
-
-class Card {
-    constructor(cardType, cost, reward, phase, image = None) {
-        this.cardType = cardType // Resource, Appliance, Wolf, VP, Coins
-        this.cost = cost // Cost object
-        this.reward = reward // Array of str 
-        this.phase = phase // 1, 2, 3
-        this.image = image // Path
-    }
-}
-
-class Inventory {
-    constructor(resource = None, resource_amt = None, coin_amt = None) {
-        this.resource = resource // Array of str, HASH ME PLS
-        this.resource_amt = resource_amt // Array of int, HASH ME PLS
-        this.coin_amt = coin_amt // Int
-    }
-}
-
-brick_card = Card("Resource", Inventory(), Inventory(["Brick"], [1]), 1);
-brick_2_card = Card("Resource", Inventory([], [], 1), Inventory(["Brick"], [2]));
