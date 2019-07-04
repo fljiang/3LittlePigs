dc3472903e5a17dbb33414432681113d3b336fca
Melody Cheng <melodycheng99@gmail.com>
2019-07-04 17:10:26 -0400

    changed formatting of stats

--- src/components/stats/statsComponent.js
+++ src/components/stats/statsComponent.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Box, List, ListItem, ListItemText, Typography } from '@material-ui/core';
-import coin from './../../img/icons/vp_icon.png';
+import coin from './../../img/icons/coin_icon.png';
 import brick from './../../img/icons/brick_icon.png';
 import stick from './../../img/icons/stick_icon.png';
 import stone from './../../img/icons/stone_icon.png';
+import mud from './../../img/icons/mud_icon.png';
 import wolf from './../../img/icons/wolf_icon.png';
 import './statsComponent.css';
 
 class Stats extends Component {
   render() {
     return (
-      <Box border={1} width="15%">
+      <Box border={1} width="35%">
         <List>
           <ListItem>
             <img src={coin} alt="" style={{ width: 25, height: 25 }} />
@@ -31,47 +32,54 @@ class Stats extends Component {
             />
           </ListItem>
           <ListItem>
-            <img src={brick} alt="" style={{ width: 25, height: 25 }} />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="SubKey">Bricks</Typography>}
-            />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="Value">0</Typography>}
-            />
+            <ListItem style={{ marginTop: -20, marginBottom: -20, marginLeft: -15 }}>
+              <img src={brick} alt="" style={{ width: 25, height: 32.5 }} />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="SubKey">Bricks</Typography>}
+              />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="Value">0</Typography>}
+              />
+            </ListItem>
+
+            <ListItem style={{ margin: -15 }}>
+              <img src={mud} alt="" style={{ width: 25, height: 32.5 }} />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="SubKey">Mud</Typography>}
+              />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="Value">0</Typography>}
+              />
+            </ListItem>
           </ListItem>
           <ListItem>
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="SubKey">Mud</Typography>}
-            />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="Value">0</Typography>}
-            />
-          </ListItem>
-          <ListItem>
-            <img src={stick} alt="" style={{ width: 25, height: 25 }} />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="SubKey">Sticks</Typography>}
-            />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="Value">0</Typography>}
-            />
-          </ListItem>
-          <ListItem>
-            <img src={stone} alt="" style={{ width: 25, height: 25 }} />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="SubKey">Stones</Typography>}
-            />
-            <ListItemText 
-              disableTypography
-              primary={<Typography className="Value">0</Typography>}
-            />
+            <ListItem style={{ marginTop: -20, marginBottom: -20, marginLeft: -15 }}>
+              <img src={stick} alt="" style={{ width: 25, height: 32.5 }} />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="SubKey">Sticks</Typography>}
+              />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="Value">0</Typography>}
+              />
+            </ListItem>
+
+            <ListItem style={{ margin: -15 }}>
+              <img src={stone} alt="" style={{ width: 25, height: 32.5 }} />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="SubKey">Stones</Typography>}
+              />
+              <ListItemText 
+                disableTypography
+                primary={<Typography className="Value">0</Typography>}
+              />
+            </ListItem>
           </ListItem>
 
           <ListItem>
@@ -86,7 +94,7 @@ class Stats extends Component {
           </ListItem>
 
           <ListItem>
-            <img src={wolf} alt="" style={{ width: 25, height: 25 }} />
+            <img src={wolf} alt="" style={{ width: 25, height: 32.5 }} />
             <ListItemText 
               disableTypography
               primary={<Typography className="Key">Wolves</Typography>}
