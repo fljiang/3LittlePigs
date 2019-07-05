cd8635bc44c0b4bcee6c410add684cc52327e060
Melody Cheng <melodycheng99@gmail.com>
2019-07-04 20:47:51 -0400

    continued board component

--- src/components/board/boardComponent.js
+++ src/components/board/boardComponent.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Box, List, ListItem, ListItemText, Typography } from '@material-ui/core';
 import brick from './../../img/icons/brick_icon.png';
+import vp_3 from './../../img/board/vp_3.png';
+import vp_5 from './../../img/board/vp_5.png';
+import vp_7 from './../../img/board/vp_7.png';
 import './boardComponent.css';
 
 const Board = ({ }) => (
   <Box border={1} width="90%">
     <List>
       <ListItem>
-        <img src={brick} alt="" style={{ width: 50, height: 65 }} />
+        <img src={brick} alt="" style={{ width: 100, height: 130 }} />
         <ListItemText 
           disableTypography
           primary={<Typography className="HouseName">Billy's House</Typography>}
         />
       </ListItem>
 
-      {/* <ListItem>
-        <img src={brick} alt="" style={{ width: 50, height: 65 }} />
-        <ListItemText 
-          disableTypography
-          primary={<Typography className="HouseName">Billy's House</Typography>}
-        />
-      </ListItem> */}
+      <ListItem>
+        <img src={vp_3} alt="" style={{ width: 130, height: 100 }} />
+        <img src={vp_5} alt="" style={{ width: 130, height: 100 }} />
+        <img src={vp_7} alt="" style={{ width: 130, height: 100 }} />
+      </ListItem>
     </List>
   </Box>
 );
