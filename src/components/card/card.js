57ea38bb49fc10fc86de835bcef0df2bd00a6a21
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2019-08-19 20:00:17 -0400

    WAOW ISS CARDS

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,9 +4,22 @@ import stick from './../../img/stick.png';
 import stick_2 from './../../img/stick._2png';
 import stone from './../../img/stone.png';
 import stone_2 from './../../img/stone_2.png';
+import water from './../../img/water.png' // needs 2
 import stone_or_stick from './../../img/stone_or_stick.png';
 import mud from './../../img/mud.png';
-import water from './../../img/water.png'
 import wolf_brick from './../../img/wolf_brick.png'; // needs all 3
 import glass from './../../img/glass.png';
+
+// brick => clay
+// stick => lumber
+// stone => stone
+// water => ore
+
+// metal => press
+// grass => loom
+// mud => glass
+
+// glass => apothecary
+// spoon => scriptorium
+// pot => workshop
 
