b8c46658ae7ecd07b26d2e54d5766172a6ab160d
Melody Cheng <melodycheng99@gmail.com>
2020-03-25 00:17:30 -0400

    Fix loading issues

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ const reducer = (
     action
 ) => {
     switch (action.type) {
+        case 'GET_CARDS':
+            return { ...state, loading: true };
         case 'HIDE_CARDS':
             return { ...state, cards: null };
         case 'CARDS_RECEIVED':
-            return { ...state, cards: action.json };
+            return { ...state, loading: false, cards: action.json };
         default:
             return state;
     }
