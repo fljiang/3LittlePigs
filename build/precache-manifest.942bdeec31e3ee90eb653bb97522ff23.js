7a83a5c1e683fc1b9f598f0c983ecc35dc7cb593
Felicia Jiang <fljiang@edu.uwaterloo.ca>
2021-05-21 15:13:32 -0400

    bug fix

---
+++ build/precache-manifest.942bdeec31e3ee90eb653bb97522ff23.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3068b192482e6633e675a62dd6a7bbae",
+    "url": "/3LittlePigs/index.html"
+  },
+  {
+    "revision": "65e6c2f1274dd2251f9e",
+    "url": "/3LittlePigs/static/css/main.729f4ff2.chunk.css"
+  },
+  {
+    "revision": "c52fcd57f5f8d99c9195",
+    "url": "/3LittlePigs/static/js/2.14b1476b.chunk.js"
+  },
+  {
+    "revision": "2f241e1557f416875455989ad74501f5",
+    "url": "/3LittlePigs/static/js/2.14b1476b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65e6c2f1274dd2251f9e",
+    "url": "/3LittlePigs/static/js/main.864b2faa.chunk.js"
+  },
+  {
+    "revision": "2e3e0c697d01ffbbf816",
+    "url": "/3LittlePigs/static/js/runtime-main.d9650be0.js"
+  },
+  {
+    "revision": "3b3cdfde361a8b42d63d14354126df87",
+    "url": "/3LittlePigs/static/media/apple.3b3cdfde.png"
+  },
+  {
+    "revision": "dd4c653342a0100e22c2567499f4455e",
+    "url": "/3LittlePigs/static/media/apple_icon.dd4c6533.png"
+  },
+  {
+    "revision": "65b86e3ce838736fb1e3ea4be28395af",
+    "url": "/3LittlePigs/static/media/brick.65b86e3c.png"
+  },
+  {
+    "revision": "d6fc43dabd59c2580266f42427d65e50",
+    "url": "/3LittlePigs/static/media/brick_icon.d6fc43da.png"
+  },
+  {
+    "revision": "652834032c10ce1b724c35c70e333876",
+    "url": "/3LittlePigs/static/media/brick_or_mud.65283403.png"
+  },
+  {
+    "revision": "d7ece2a8a5d647978af5b0b75e4c981f",
+    "url": "/3LittlePigs/static/media/coin_icon.d7ece2a8.png"
+  },
+  {
+    "revision": "72bee46b887897d49527bcffd338ea73",
+    "url": "/3LittlePigs/static/media/flower.72bee46b.png"
+  },
+  {
+    "revision": "b8a74c9f21544b04c3fd8c6e85310916",
+    "url": "/3LittlePigs/static/media/flower_icon.b8a74c9f.png"
+  },
+  {
+    "revision": "168e283139cbb134399ad8f10511b8a7",
+    "url": "/3LittlePigs/static/media/glass_flower.168e2831.png"
+  },
+  {
+    "revision": "a43d78a10805e3b90d91c629a9eade9b",
+    "url": "/3LittlePigs/static/media/glass_icon.a43d78a1.png"
+  },
+  {
+    "revision": "d5471972acd2d8be0ff4b283b4b42d94",
+    "url": "/3LittlePigs/static/media/market_both.d5471972.png"
+  },
+  {
+    "revision": "6ce7b501a14a3ad6543a07bcc43c3f67",
+    "url": "/3LittlePigs/static/media/market_left.6ce7b501.png"
+  },
+  {
+    "revision": "2d7dfc43b978e9df9fafb316d8143519",
+    "url": "/3LittlePigs/static/media/market_right.2d7dfc43.png"
+  },
+  {
+    "revision": "1218ba3539659bc84c7bc598a1dfefd9",
+    "url": "/3LittlePigs/static/media/mud.1218ba35.png"
+  },
+  {
+    "revision": "68d33d034a154ec9f7e239fa4b1d6348",
+    "url": "/3LittlePigs/static/media/mud_icon.68d33d03.png"
+  },
+  {
+    "revision": "f28da90a65ed45ad371dac0a00b72544",
+    "url": "/3LittlePigs/static/media/pot_icon.f28da90a.png"
+  },
+  {
+    "revision": "1f19d63aa055ca5543181e657c455fc3",
+    "url": "/3LittlePigs/static/media/pot_water.1f19d63a.png"
+  },
+  {
+    "revision": "f95b4347033452cac05c72622a469fbf",
+    "url": "/3LittlePigs/static/media/spoon_apple.f95b4347.png"
+  },
+  {
+    "revision": "4eadfe94dc4e404e60c2b9f235ae9fe0",
+    "url": "/3LittlePigs/static/media/spoon_icon.4eadfe94.png"
+  },
+  {
+    "revision": "03075c872470b5bd01f59eaf6d530c34",
+    "url": "/3LittlePigs/static/media/stick.03075c87.png"
+  },
+  {
+    "revision": "25988e403f51e3e2b41bd8b138ae68ae",
+    "url": "/3LittlePigs/static/media/stick_icon.25988e40.png"
+  },
+  {
+    "revision": "99835989e58db8e2ad148d9239e75473",
+    "url": "/3LittlePigs/static/media/stone.99835989.png"
+  },
+  {
+    "revision": "7cce1c1495ceb1261e32cd7dcfd686c6",
+    "url": "/3LittlePigs/static/media/stone_icon.7cce1c14.png"
+  },
+  {
+    "revision": "7e3ed856f14284b17415a8c6f8dbbae6",
+    "url": "/3LittlePigs/static/media/stone_or_stick.7e3ed856.png"
+  },
+  {
+    "revision": "1e16b21614a4a499ab04fcb16618f84a",
+    "url": "/3LittlePigs/static/media/vp_2_brown.1e16b216.png"
+  },
+  {
+    "revision": "4a24e6023033d284f7c2d143c0d81eb6",
+    "url": "/3LittlePigs/static/media/vp_2_pink.4a24e602.png"
+  },
+  {
+    "revision": "18d8077ada15bda1f1db34b12646f62f",
+    "url": "/3LittlePigs/static/media/vp_3.18d8077a.png"
+  },
+  {
+    "revision": "6db80a771dc361fa1d7852daa0318eb3",
+    "url": "/3LittlePigs/static/media/vp_3_yellow.6db80a77.png"
+  },
+  {
+    "revision": "8f8dde524dd4a1c83ae5dc63c5f1c809",
+    "url": "/3LittlePigs/static/media/vp_5.8f8dde52.png"
+  },
+  {
+    "revision": "27cf558d555be2f21259213380de4f60",
+    "url": "/3LittlePigs/static/media/vp_7.27cf558d.png"
+  },
+  {
+    "revision": "6b3c5a78f044953922cf2ee596411b22",
+    "url": "/3LittlePigs/static/media/vp_icon.6b3c5a78.png"
+  },
+  {
+    "revision": "14da0fd6e84ea00f17997a8ab133bf21",
+    "url": "/3LittlePigs/static/media/water.14da0fd6.png"
+  },
+  {
+    "revision": "a3d6bc2fd6337dd52a34f9e51ebfc7f7",
+    "url": "/3LittlePigs/static/media/water_icon.a3d6bc2f.png"
+  },
+  {
+    "revision": "fcfca1c02a96229bb7e6ad53a57ac47e",
+    "url": "/3LittlePigs/static/media/wolf_brick.fcfca1c0.png"
+  },
+  {
+    "revision": "9202651475d96000a6d253249b23e4cd",
+    "url": "/3LittlePigs/static/media/wolf_icon.92026514.png"
+  },
+  {
+    "revision": "3df5da28eafbcde5a65e762a505717cf",
+    "url": "/3LittlePigs/static/media/wolf_stick.3df5da28.png"
+  },
+  {
+    "revision": "09df72222f094791125db6b2c5dd55d0",
+    "url": "/3LittlePigs/static/media/wolf_stone.09df7222.png"
+  }
+]);
